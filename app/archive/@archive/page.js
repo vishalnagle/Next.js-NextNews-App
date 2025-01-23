bd769ea9c4c4d41839b9b7755fccaa29a72ef487
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-23 22:53:43 +0530

    parallel routes functionality added

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,3 +1,15 @@
-export default function ArchivePage(){
-    return <h1>Archive Page</h1>
+import { getAvailableNewsYears } from "@/lib/news"
+import Link from "next/link";
+
+export default function ArchivePage() {
+    const links = getAvailableNewsYears();
+    return <header id="archive-header">
+        <nav>
+            <ul>
+                {links.map((link) => (
+                    <Link href={`/archive/${link}`}>{link}</Link>
+                ))}
+            </ul>
+        </nav>
+    </header>
 }
