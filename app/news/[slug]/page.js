3ad5ddc1b14e7b092338ddcea8dc7eb21f146696
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-23 18:59:47 +0530

    News and newsdetail components styling added with dummy data

---
+++ app/news/[slug]/page.js
@@ -0,0 +1,15 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+
+export default function NewsDetailPage({ params }) {
+    const newsSlug = params.slug;
+    const newsItem = DUMMY_NEWS.find((item) => item.slug === newsSlug)
+    return <article className="news-article">
+        <header>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+            <h1>News Detail Page</h1>
+            <time dateTime={newsItem.date}>{newsItem.date}</time>
+        </header>
+        <p>{newsItem.content}</p>
+    </article>
+}
--- app/news/page.js
+++ app/news/page.js
@@ -1,18 +1,16 @@
 import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
 
-export default function NewsPage(){
+export default function NewsPage() {
     return <>
-    <h1>News Page</h1>
-    <ul>
-        <li>
-            <Link href="/news/1">First News</Link>
-        </li>
-        <li>
-            <Link href="/news/2">Second News</Link>
-        </li>
-        <li>
-            <Link href="/news/3">Third News</Link>
-        </li>
-    </ul>
+        <h1>News Page</h1>
+        <ul className="news-list">
+            {DUMMY_NEWS.map((newsItem) => (<li key={newsItem.id}>
+                <Link href={`/news/${newsItem.slug}`}>
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                    <span>{newsItem.title}</span>
+                </Link>
+            </li>))}
+        </ul>
     </>
 }
